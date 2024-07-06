8e0acb9727e5e4878d70844c8a454071f8fd4bd4
Darren <darrencomador@gmail.com>
2024-07-06 14:30:34 +0800

    added other page and delete welcome blade

---
+++ resources/views/expenses.blade.php
@@ -0,0 +1,29 @@
+{{-- Header --}}
+@include('include/header')
+
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+	<!-- Content Header (Page header) -->
+	<div class="content-header">
+		<div class="container-fluid">
+			<div class="row mb-2">
+				<div class="col-sm-6">
+					<h1 class="m-0">Expenses</h1>
+				</div>
+			</div>
+		</div><!-- /.container-fluid -->
+	</div>
+	<!-- /.content-header -->
+
+	<!-- Main content -->
+	<section class="content">
+		<div class="container-fluid">
+			
+		</div><!--/. container-fluid -->
+	</section>
+	<!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+
+{{-- Footer --}}
+@include('include/footer')
